df6b158b0a1f3160d73c902f57f9695c9e0de075
Two-Jay <djeeee1272@gmail.com>
2023-05-03 21:31:23 +0900

    day 00 ex00 end

---
+++ Day 0 - Starting/ex00/Hello.py
@@ -0,0 +1,21 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+ft_list.pop(1)
+ft_list.append("World!")
+
+ft_tuple = ("Hello", "Korea!")
+
+ft_set.discard("tutu!")
+ft_set.add("Seoul!")
+sorted(ft_set)
+
+ft_dict["Hello"] = "42Seoul!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
+
