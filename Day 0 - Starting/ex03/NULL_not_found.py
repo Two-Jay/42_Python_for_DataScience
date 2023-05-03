a715cb21048078bfd69ecee2c6cec0bbcd27f2ee
Two-Jay <djeeee1272@gmail.com>
2023-05-03 22:29:32 +0900

    day 00 ex 03 end

---
+++ Day 0 - Starting/ex03/NULL_not_found.py
@@ -0,0 +1,17 @@
+import math
+
+def NULL_not_found(object : any) -> int:
+    checked = type(object)
+    if object == None:
+        print(f"Nothing : {object} {checked}")
+    elif type(object) == float and math.isnan(object):
+        print(f"cheese : {object} {checked}")
+    elif type(object) == int and object == 0:
+        print(f"Zero : {object} {checked}")
+    elif type(object) == str and object == '':
+        print(f"Empty : {object} {checked}")
+    elif type(object) == bool and object == False:
+        print(f"Fake : {object} {checked}")
+    else:
+        print("Type not found")
+        return 1
